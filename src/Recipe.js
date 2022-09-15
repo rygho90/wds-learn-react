d5608e93bf3fac9d44216af492bc1d4818f5bf53
Ryan <rygho90@gmail.com>
2022-09-15 13:10:35 -0400

    Complete Lesson 11

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,7 +1,8 @@
 import React from "react";
+import IngredientList from "./IngredientList";
 
 export default function Recipe(props) {
-  const { name, cookTime, servings, instructions } = props;
+  const { name, cookTime, servings, instructions, ingredients } = props;
 
   return (
     <div>
@@ -23,6 +24,10 @@ export default function Recipe(props) {
       <div>
         <span>Instructions:</span>
         <div>{instructions}</div>
+      </div>
+      <div>
+        <span>Ingredients:</span>
+        <div><IngredientList ingredients={ingredients} /></div>
       </div>
     </div>
   );
